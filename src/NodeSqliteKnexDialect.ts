65c8e927efdda657d678e6cd7929520da8b82443
uki00a <uki00a@gmail.com>
2025-04-29 18:34:40 +0900

    fix: improve `NodeSqliteKnexDialect`

--- src/NodeSqliteKnexDialect.ts
+++ src/NodeSqliteKnexDialect.ts
@@ -24,6 +24,23 @@
  * SOFTWARE.
  */
 import { MonkeyPatchable } from "@mikro-orm/knex";
+import { DatabaseSync } from "node:sqlite";
+
+/**
+ * @see {@link https://github.com/knex/knex/blob/9bd12999907436c2ef51f786df09a9a7e8931cca/lib/dialects/better-sqlite3/index.js#L14-L17}
+ */
+interface DriverOptions {
+  nativeBinding: string;
+  readonly: boolean;
+}
+function createDatabaseSync(
+  database: string,
+  options: DriverOptions,
+): DatabaseSync {
+  return new DatabaseSync(database, {
+    readOnly: options.readonly,
+  });
+}
 
 export class NodeSqliteKnexDialect
   extends MonkeyPatchable.BetterSqlite3Dialect {
@@ -32,6 +49,65 @@ export class NodeSqliteKnexDialect
   }
 
   _driver() {
-    return require("node:sqlite");
+    return createDatabaseSync;
+  }
+
+  /**
+   * Implemented based on the following sources:
+   *
+   * * {@link https://github.com/knex/knex/blob/9bd12999907436c2ef51f786df09a9a7e8931cca/lib/dialects/better-sqlite3/index.js#L28 | Client_BetterSQLite3#_query}
+   * * {@link https://github.com/knex/knex/blob/9bd12999907436c2ef51f786df09a9a7e8931cca/lib/dialects/sqlite3/index.js#L121 | Client_SQLite3#_query}
+   *
+   * They are licensed as follows:
+   *
+   * Copyright (c) 2013-present Tim Griesser
+   *
+   * Permission is hereby granted, free of charge, to any person
+   * obtaining a copy of this software and associated documentation
+   * files (the "Software"), to deal in the Software without
+   * restriction, including without limitation the rights to use,
+   * copy, modify, merge, publish, distribute, sublicense, and/or sell
+   * copies of the Software, and to permit persons to whom the
+   * Software is furnished to do so, subject to the following
+   * conditions:
+   *
+   * The above copyright notice and this permission notice shall be
+   * included in all copies or substantial portions of the Software.
+   *
+   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+   * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+   * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+   * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+   * OTHER DEALINGS IN THE SOFTWARE.
+   */
+  async _query(connection, obj) {
+    let callMethod: "all" | "run";
+    switch (obj.method) {
+      case "insert":
+      case "update":
+        callMethod = obj.returning ? "all" : "run";
+        break;
+      case "counter":
+      case "del":
+        callMethod = "run";
+        break;
+      default:
+        callMethod = "all";
+    }
+    const statement = connection.prepare(obj.sql);
+    const response = statement[callMethod](...obj.bindings);
+    if (callMethod === "all") {
+      obj.response = response;
+    } else {
+      obj.response = response;
+      obj.context = {
+        lastID: response.lastInsertRowid,
+        changes: response.changes,
+      };
+    }
+    return obj;
   }
 }
