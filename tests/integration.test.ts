942fcc8cf7e7078cd192be6866296585f9644efb
uki00a <uki00a@gmail.com>
2025-06-02 23:35:28 +0900

    fix: use `node:fs/promises`

--- tests/integration.test.ts
+++ tests/integration.test.ts
@@ -1,4 +1,5 @@
 import assert from "node:assert/strict";
+import { rm } from "node:fs/promises";
 
 import { MikroORM } from "@uki00a/mikro-orm-node-sqlite-driver";
 
@@ -46,6 +47,6 @@ Deno.test("integration", async (t) => {
   });
   await orm.close();
   if (config.dbName) {
-    await Deno.remove(config.dbName);
+    await rm(config.dbName);
   }
 });
